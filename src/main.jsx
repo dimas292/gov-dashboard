6182d2a931c3ad703795fba2e9967b4141540cfb
Dimssss <dimasixc22@gmail.com>
2026-08-24 09:40:29 +0700

    fix: import css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "../../assets/css/style.css";
+import "../assets/css/style.css";
 
 const savedTheme = localStorage.getItem("govassist-theme");
 if (savedTheme === "dark" || savedTheme === "light") {
